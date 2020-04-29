68207e45c5d0701f400be9f45b29c31398fb2297
Rico-HBChen <chenhongbo_whut@163.com>
2020-04-29 21:36:25 +0800

    1.2.6列表作业

---
+++ 1.2.6listStudy.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+
+lst = list("python")
+lst.extend(list("rust"))
+print(lst)
+print("列表升序排列如下：")
+lst.sort()
+print(lst)
+
+#使用空列表记录重复元素的后一个的索引
+idxlst = []
+for i in range(len(lst)-1):
+    if lst[i] == lst[i+1]:
+        idxlst.append(i+1)
+#判断是否有充分元素，如果有，就删掉
+if idxlst:
+    for i in range(len(idxlst)):
+        lst.remove(lst[idxlst[i]])
+
+print("删除列表重复元素后如下")
+print(lst)
