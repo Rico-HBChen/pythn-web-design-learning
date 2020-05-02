18c53c3a045814c2dfa3582ddd55e252d3e91632
Rico-HBChen <chenhongbo_whut@163.com>
2020-05-02 16:40:24 +0800

    1.2.8字典作业

--- 1.2.8字典作业.py
+++ 1.2.8字典作业.py
@@ -3,5 +3,5 @@
 contryandcity = {'中国':'北京','法国':'巴黎','美国':'纽约',
 '英国':'伦敦','日本':'东京'}
 
-contry = input("请问需要查阅哪个国家的首都：")
+contry = input("请问需要查阅哪个国家的首都（可查询中国、法国、美国、英国、日本）：")
 print("{0}的首都是{1}".format(contry,contryandcity[contry]))
