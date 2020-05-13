5a9c3481b25745cf1d9954bf4b440d469c4a1c37
Rico-HBChen <chenhongbo_whut@163.com>
2020-05-13 12:51:30 +0800

    1.3.2作业修改

--- 1.3.2条件判断作业.py
+++ 1.3.2条件判断作业.py
@@ -10,3 +10,21 @@ elif idata.isalpha():
     print(idata+"@python")
 else:
     print(idata)
+
+'''
+习题课后查找资料对比判断字符串是否为数字字符串函数
+isdigit()
+True: Unicode数字，byte数字（单字节），全角数字（双字节），罗马数字
+False: 汉字数字
+Error: 无
+
+isdecimal()
+True: Unicode数字，，全角数字（双字节）
+False: 罗马数字，汉字数字
+Error: byte数字（单字节）
+
+isnumeric()
+True: Unicode数字，全角数字（双字节），罗马数字，汉字数字
+False: 无
+Error: byte数字（单字节）
+'''
