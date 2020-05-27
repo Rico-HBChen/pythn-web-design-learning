abcd009f69b04279c171e5f44db0ed5c98bc23ed
Rico-HBChen <chenhongbo_whut@163.com>
2020-05-27 21:13:40 +0800

    2.2.2作业

---
+++ 2.2.2作业.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.length = 0
+
+    def __getattr__(self,name):
+        if name == 'size':
+            return self.width,self.length
+        else:
+            raise AttributeError
+
+    def __setattr__(self,name,value):
+        try:
+            if name == 'size':
+                self.width,self.length = value
+            else:
+                self.__dict__[name] = value
+        except TypeError:
+            print('size需要输入长和宽，中间用逗号隔开。')
+
+'''
+    def __setattr__(self,name,value):
+        if name == 'size':
+            x,y = 0,0
+            x,y = value
+            assert y
+            self.width,self.length = value
+        else:
+            self.__dict__[name] = value
+'''
+
+
+rect = Rectangle()
+rect.width = 5
+rect.length =3
+rect.size = 20
+print(rect.size)
